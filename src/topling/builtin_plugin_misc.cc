bf5b094b31b1fa55939c570e4e169011c05b9d95
leipeng <rockeet@gmail.com>
2021-06-19 10:04:29 +0800

    remove reverse dependency "plugin_repo"

--- src/topling/builtin_plugin_misc.cc
+++ src/topling/builtin_plugin_misc.cc
@@ -138,7 +138,6 @@ struct DBOptions_Json : DBOptions {
     Update(js, repo);
   }
   void Update(const json& js, const SidePluginRepo& repo) {
-    this->plugin_repo = &repo;
     ROCKSDB_JSON_OPT_PROP(js, create_if_missing);
     ROCKSDB_JSON_OPT_PROP(js, create_missing_column_families);
     ROCKSDB_JSON_OPT_PROP(js, error_if_exists);
