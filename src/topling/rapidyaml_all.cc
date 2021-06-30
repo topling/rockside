c12da2258cdb609296c0aed7d918fe9e6a534a15
leipeng <rockeet@gmail.com>
2021-06-30 17:24:33 +0800

    Add src/topling/rapidyaml_all.cc

---
+++ src/topling/rapidyaml_all.cc
@@ -0,0 +1,22 @@
+#ifdef SIDE_PLUGIN_WITH_YAML
+#include <ryml.hpp>
+#include <c4/std/string.hpp>
+
+#include <ext/c4core/src/c4/base64.cpp>
+#include <ext/c4core/src/c4/char_traits.cpp>
+#include <ext/c4core/src/c4/error.cpp>
+#include <ext/c4core/src/c4/format.cpp>
+#include <ext/c4core/src/c4/language.cpp>
+#include <ext/c4core/src/c4/memory_resource.cpp>
+#include <ext/c4core/src/c4/memory_util.cpp>
+#include <ext/c4core/src/c4/time.cpp>
+#include <src/c4/yml/common.cpp>
+#include <src/c4/yml/parse.cpp>
+#include <src/c4/yml/preprocess.cpp>
+#include <src/c4/yml/tree.cpp>
+
+#endif
+
+void rapidyaml_all_sig() {
+    // do nothing
+}
