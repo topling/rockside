34f9ae12a50b4365ab31ba47ddd4ec60f1983de6
mytrygithub <lifz_2015@163.com>
2021-08-25 17:35:51 +0800

    delete no use parameter

--- src/topling/builtin_plugin_misc.cc
+++ src/topling/builtin_plugin_misc.cc
@@ -1953,8 +1953,7 @@ static std::string Json_DB_OneSST(const DB& db, ColumnFamilyHandle* cfh0,
 //&DB::Properties::kAggregatedTablePropertiesAtLevel
 
 //&DB::Properties::kBlockCacheEntryStats no response ignore
-static string CFPropertiesMetric(const DB& db, ColumnFamilyHandle* cfh,
-                             json& js) {
+static string CFPropertiesMetric(const DB& db, ColumnFamilyHandle* cfh) {
   static const string* int_properties[] = {
     &DB::Properties::kNumImmutableMemTable,
     &DB::Properties::kNumImmutableMemTableFlushed,
