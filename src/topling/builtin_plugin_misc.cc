756ae1d5afa9718bbc8ff158cc7f4d0d38713655
mytrygithub <lifz_2015@163.com>
2021-08-25 10:46:21 +0800

    add paramter for some Properties
    rebase master

--- src/topling/builtin_plugin_misc.cc
+++ src/topling/builtin_plugin_misc.cc
@@ -1991,6 +1991,37 @@ static string CFPropertiesMetric(const DB& db, ColumnFamilyHandle* cfh) {
         replace_char(name);
         oss|name|" "|value|"\n";
       }
+
+      if (iter.first == &DB::Properties::kBlockCacheEntryStats) {
+        oss|*iter.first|"\n";
+        std::map<std::string, std::string> value;
+        if (const_cast<DB&>(db).GetMapProperty(cfh, *iter.first, &value)) {
+          //oss|"name:"|*iter.first|"\n";
+          for (auto const v_iter:value) {
+            oss|name|":"|v_iter.first|" "|v_iter.second|"\n";
+          }
+        }
+      }
+
+      auto add_param_get_value=[&iter,&db,&cfh,&oss](const string *prefix) {
+        if (iter.first == prefix) {
+          const int num_levels = const_cast<DB&>(db).NumberLevels(cfh);
+          for (int level = -1; level < num_levels; level++) {
+            string value;
+            string key = *iter.first;
+            key.append(std::to_string(level));
+            oss|key|"\n";
+            if (const_cast<DB&>(db).GetProperty(cfh, key, &value)) {
+              //oss|"name:"|*iter.first|"\n";
+              oss|key|" "|value|"\n";
+            }
+          }
+        }
+      };
+
+      add_param_get_value(&DB::Properties::kNumFilesAtLevelPrefix);
+      add_param_get_value(&DB::Properties::kCompressionRatioAtLevelPrefix);
+      add_param_get_value(&DB::Properties::kAggregatedTablePropertiesAtLevel);
     }
   };
   for (auto const key:prefix_properties) { add_prefix_properties(key); }
